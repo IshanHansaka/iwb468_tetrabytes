91ba7198880bf257ad5dd7025a1dff90891a7fbe
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-10-20 19:15:43 +0530

    chore: update db

--- backend/resources/laptop_db.sql
+++ backend/resources/laptop_db.sql
@@ -65,6 +65,38 @@ VALUES (5, 'MSI', 'Modern 15 B13M', 'Intel® Core™ i5-1335U (12MB Cache, up to
         '16GB DDR4 3200MHZ (Onboard)', '512GB NVME GEN4 M.2 SSD', '15.6” 1080P Display IPS LEVEL', 'Intel Iris Xe Graphics', 
         '1.7kg', '39.3WHrs', 'https://www.nanotek.lk/uploads/product/2074-20240808155453-MODERN%20B12M.png');
 
+-- Inserting laptop06
+INSERT INTO laptop (id, brand, model, processor, ram, storage, display, gpu, weight, battery, image_link)
+VALUES (6, 'Apple', 'MacBook Air M3 8GB 256GB 13" (2024)', 'Apple M3 8-core CPU 8-core GPU', 
+        '8GB unified memory', '256GB SSD', '13.6-inch Liquid Retina display with True Tone²', 
+        'Apple 8-core GPU', '1.24kg', '52.6 WHrs', 'https://www.nanotek.lk/uploads/product/2018-20240420061514-Untitled-1%20(2).png');
+
+-- Inserting laptop07
+INSERT INTO laptop (id, brand, model, processor, ram, storage, display, gpu, weight, battery, image_link)
+VALUES (7, 'Acer', 'Aspire A15-51P-39HQ', 'Intel® Core™ i3-100U Processor (10M Cache, up to 4.7GHz)', 
+        '8GB LPDDR5 4800MHz', '512GB NVME SSD', '15.6" FHD (1920x1080) IPS', 
+        'Intel® UHD Graphics', '1.8kg', '50WHrs', 'https://www.nanotek.lk/uploads/product/2503-20240903132009-ACER%20ASPIRE%20A515-58P%2013TH%20GEN.png');
+
+-- Inserting laptop08
+INSERT INTO laptop (id, brand, model, processor, ram, storage, display, gpu, weight, battery, image_link)
+VALUES (8, 'Dell', 'Inspiron 3520', 'Intel® Core™ i5-1235U (12M Cache, up to 4.40GHz)', 
+        '8GB DDR4 2666MHz', '512GB NVME M.2 SSD', '15.6" FHD (1920x1080) IPS-Level', 
+        'Intel UHD Graphics', '1.65kg', '41WHrs', 'https://www.nanotek.lk/uploads/product/1638-20240924175012-dell-inspiron-3520-laptop-396-cm-156-full-hd-intel-core-i5-i5-1235u-8-gb-ddr4-sdram-512-gb-ssd-wi-fi-6-80211ax-windows-11-black.png');
+
+-- Inserting laptop09
+INSERT INTO laptop (id, brand, model, processor, ram, storage, display, gpu, weight, battery, image_link)
+VALUES (9, 'HP', 'Victus 15 FB2082WM', 'AMD Ryzen™ 5 8645HS (6 Core, 12 Threads-16M Cache, up to 5 GHz)', 
+        '8GB DDR5 4800MHz', '512GB GEN4 NVME M.2 SSD', 
+        '15.6" FHD IPS LED Display 144HZ Thin Bezel (300 nits)', 
+        'NVIDIA® GeForce RTX 4050 6GB GDDR6', '2.29kg', '70 WHr', 
+        'https://www.nanotek.lk/uploads/product/1426-20240809183526-HP%20Victus%2015%202023.png');
+
+INSERT INTO laptop (id, brand, model, processor, ram, storage, display, gpu, weight, battery, image_link)
+VALUES (11, 'HP', 'Victus 15 FB2082WM', 'AMD Ryzen™ 5 8645HS (6 Core, 12 Threads-16M Cache, up to 5 GHz)', 
+        '8GB DDR5 4800MHz', '512GB GEN4 NVME M.2 SSD', '15.6" FHD IPS LED Display 144HZ Thin Bezel (300 nits)', 
+        'NVIDIA® GeForce RTX 4050 6GB GDDR6 (TDP 75W)', '2.29KG', '70 WHr', 'https://www.nanotek.lk/uploads/product/1426-20240809183526-HP%20Victus%2015%202023.png');
+
+
 -- Inserting shop01
 INSERT INTO shop (name) VALUES ('shop01');
 
@@ -81,7 +113,12 @@ VALUES
 (2, 1, 304000, '2 Year', TRUE, '2024-10-20'),
 (3, 1, 1395000, '2 Year', FALSE, '2024-10-20'),
 (4, 1, 1589000, '2 Year', TRUE, '2024-10-20'),
-(5, 1, 215000, '2 Year', TRUE, '2024-10-20');
+(5, 1, 215000, '2 Year', TRUE, '2024-10-20'),
+(6, 1, 349000, '1 Year', TRUE, '2024-10-20'),
+(7, 1, 156000, '2 Year', TRUE, '2024-10-20'),
+(8, 1, 179000, '3 Year', TRUE, '2024-10-20'),
+(9, 1, 499000, '2 Year', TRUE, '2024-10-20'),
+(10, 1, 289000, '1 Year', FALSE, '2024-10-20');
 
 -- For the second API set (shop02)
 INSERT INTO product (laptop_id, shop_id, price, warranty, in_stock, last_updated)
@@ -90,12 +127,23 @@ VALUES
 (2, 2, 305000, '2 Year', TRUE, '2024-10-20'),
 (3, 2, 1393000, '2 Year', TRUE, '2024-10-20'),
 (4, 2, 1585000, '2 Year', TRUE, '2024-10-20'),
-(5, 2, 210000, '1 Year', FALSE, '2024-10-20');
+(5, 2, 210000, '1 Year', FALSE, '2024-10-20'),
+(6, 2, 345000, '1 Year', TRUE, '2024-10-20'),
+(7, 2, 157000, '2 Year', TRUE, '2024-10-20'),
+(8, 2, 180000, '3 Year', TRUE, '2024-10-20'),
+(9, 2, 498000, '2 Year', TRUE, '2024-10-20'),
+(10, 2, 290000, '1 Year', FALSE, '2024-10-20');
 
 -- For the third API set (shop03)
 INSERT INTO product (laptop_id, shop_id, price, warranty, in_stock, last_updated)
 VALUES
-(2, 3, 325000, '1 Year', TRUE, '2024-10-20'),
-(3, 3, 1400000, '3 Year', TRUE, '2024-10-20'),
-(4, 3, 1600000, '3 Year', TRUE, '2024-10-20'),
-(5, 3, 218000, '2 Year', TRUE, '2024-10-20');
+(1, 3, 330000, '1 Year', TRUE, '2024-10-20'),
+(2, 3, 310000, '2 Year', TRUE, '2024-10-20'),
+(3, 3, 1398000, '3 Year', TRUE, '2024-10-20'),
+(4, 3, 1610000, '3 Year', TRUE, '2024-10-20'),
+(5, 3, 220000, '1 Year', TRUE, '2024-10-20'),
+(6, 3, 347000, '1 Year', TRUE, '2024-10-20'),
+(7, 3, 158000, '2 Year', TRUE, '2024-10-20'),
+(8, 3, 182000, '3 Year', TRUE, '2024-10-20'),
+(9, 3, 500000, '2 Year', TRUE, '2024-10-20'),
+(10, 3, 295000, '1 Year', TRUE, '2024-10-20');
