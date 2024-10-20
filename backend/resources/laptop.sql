b8b4b8640db6e6728a357366695bf5d1d721b9aa
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-10-20 12:49:32 +0530

    chore: update sql queries

--- backend/resources/laptop.sql
+++ backend/resources/laptop.sql
@@ -1,4 +1,39 @@
-USE laptop_db
+CREATE DATABASE laptop_db;
+USE laptop_db;
+
+-- creating laptop table
+CREATE TABLE laptop (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    brand VARCHAR(255),
+    model VARCHAR(255),
+    processor VARCHAR(255),
+    ram VARCHAR(255),
+    storage VARCHAR(255),
+    display VARCHAR(255),
+    gpu VARCHAR(255),
+    weight VARCHAR(255),
+    battery VARCHAR(255),
+    image_link VARCHAR(255)
+);
+
+-- creating shop table
+CREATE TABLE shop (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+-- creating product table
+CREATE TABLE product (
+    laptop_id INT,
+    shop_id INT,
+    price INT,
+    warranty VARCHAR(255),
+    in_stock BOOLEAN,
+    last_updated VARCHAR(255),
+    PRIMARY KEY (laptop_id, shop_id),
+    FOREIGN KEY (laptop_id) REFERENCES laptop(id),
+    FOREIGN KEY (shop_id) REFERENCES shop(id)
+);
 
 -- Inserting laptop01
 INSERT INTO laptop (id, brand, model, processor, ram, storage, display, gpu, weight, battery, image_link)
